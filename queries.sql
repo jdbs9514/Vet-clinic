e2a1d1515a595bb456c8832ecafe3b5288d117f3
jdbs9514 <jdbs9514@gmail.com>
2022-09-30 16:31:02 -0500

    Add the update table and the queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,48 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM ANIMALS WHERE name NOT LIKE '%Gabumon%';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > DATE '2022-01-01';
+SAVEPOINT SP01;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP01;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+/* How many animals are there? */
+SELECT COUNT(*) AS "NUMBER OF ANIMALS" FROM animals;
+
+/* How many animals have never tried to escape? */
+SELECT COUNT(*) AS "ANIMALS WHO HAVE NEVER TRIED TO ESCAPE" FROM animals WHERE escape_attempts = 0;
+
+/* What is the average weight of animals? */
+SELECT AVG(weight_kg) AS "AVERAGE WEIGHT OF ANIMALS" FROM animals;
+
+/* Who escapes the most, neutered or not neutered animals? */
+SELECT neutered, AVG(escape_attempts) AS "AVERAGE ESCAPE ATTEMPTS OF ANIMALS" FROM animals GROUP BY neutered ORDER BY AVG(escape_attempts) DESC LIMIT 1;
+
+/* What is the minimum and maximum weight of each type of animal? */
+SELECT species, MIN(weight_kg) AS "MINIMUM WEIGHT", MAX(weight_kg) AS "MAXIMUM WEIGHT" FROM animals GROUP BY species;
+
+/* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+SELECT species, AVG(escape_attempts) AS "AVERAGE ESCAPE ATTEMPTS OF ANIMALS" FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -10,3 +10,5 @@ CREATE TABLE animals (
   neutered BOOLEAN NOT NULL,
   weight_kg DECIMAL NOT NULL
 );
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
