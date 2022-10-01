b49a4fe09183a7ebd241700ff909e080c5b59820
jdbs9514 <jdbs9514@gmail.com>
2022-09-30 20:15:46 -0500

    Adding multiple tables to the vet clinic

--- schema.sql
+++ schema.sql
@@ -12,3 +12,20 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name TEXT,
+  age INT
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
